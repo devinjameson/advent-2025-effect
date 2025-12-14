b1c02841b75bc6aeb76b2f21a8304760bb34bb25
Devin Jameson <39350030+devinjameson@users.noreply.github.com>
2025-12-13 21:49:22 -0500

    day 06 part two

--- src/06/two.ts
+++ src/06/two.ts
@@ -1,7 +1,97 @@
-import { Effect } from 'effect'
+import {
+  Array,
+  Effect,
+  Match,
+  Number,
+  Option,
+  String,
+  flow,
+  pipe,
+} from 'effect'
+
+import { getLines } from '../common.js'
 
 export const solution = Effect.gen(function* () {
-  // TODO: Implement solution
+  const lines = yield* getLines('input.txt')
 
-  return 0
+  const lastLine = pipe(lines, Array.last, Option.getOrThrow)
+
+  const operations = pipe(
+    lastLine,
+    String.trim,
+    String.split(''),
+    Array.filter(flow(String.trim, String.isNonEmpty)),
+    Array.reverse,
+  )
+
+  return pipe(
+    lines,
+    Array.init,
+    Option.getOrThrow,
+    Array.map(flow(String.split(''), Array.reverse)),
+    Array.reduce<Array<Option.Option<string>>, Array<string>>(
+      Array.makeBy(String.length(lastLine), () => Option.none()),
+      (values, strings) => {
+        return Array.zipWith(values, strings, (value, string) =>
+          pipe(
+            value,
+            Option.match({
+              onNone: () => string,
+              onSome: String.concat(string),
+            }),
+            Option.some,
+          ),
+        )
+      },
+    ),
+    Array.map(Option.flatMap(Number.parse)),
+    Array.reduce<
+      { columnTotals: Array<Option.Option<number>>; columnIndex: number },
+      Option.Option<number>
+    >(
+      {
+        columnTotals: Array.makeBy(Array.length(operations), () =>
+          Option.none(),
+        ),
+        columnIndex: 0,
+      },
+      ({ columnTotals, columnIndex }, maybeNumber) => {
+        return Option.match(maybeNumber, {
+          onNone: () => ({
+            columnTotals,
+            columnIndex: Number.increment(columnIndex),
+          }),
+          onSome: (number) => {
+            const operation = Array.get(operations, columnIndex).pipe(
+              Option.getOrThrow,
+            )
+            const nextColumnTotals = Array.modify(
+              columnTotals,
+              columnIndex,
+              (columnTotal) =>
+                Match.value(operation).pipe(
+                  Match.when('*', () =>
+                    Option.match(columnTotal, {
+                      onNone: () => number,
+                      onSome: Number.multiply(number),
+                    }),
+                  ),
+                  Match.when('+', () =>
+                    Option.match(columnTotal, {
+                      onNone: () => number,
+                      onSome: Number.sum(number),
+                    }),
+                  ),
+                  Match.option,
+                ),
+            )
+            return { columnTotals: nextColumnTotals, columnIndex }
+          },
+        })
+      },
+    ),
+    ({ columnTotals }) => columnTotals,
+    Array.getSomes,
+    Array.reduce(0, (sum, value) => sum + value),
+  )
 })
